2ede13378b874f1d0353272eadf47fc99ac55eea
Nils Backe <nilsbacke@Nilss-MacBook-Pro.local>
2018-10-23 12:05:24 -0400

    initial

--- src/Course.java
+++ src/Course.java
@@ -2,19 +2,19 @@
 public class Course extends ASchoolElement {
   Instructor prof;
   IList<Student> students;
-  
+
   // creates a new Course object
   Course(String name, Instructor prof, IList<Student> students) {
     super(name);
     this.prof = prof;
     this.students = students;
   }
-  
+
   // EFFECT: adds the given student to the list of students in this course
   void addStudent(Student s) {
     this.students.addToFront(s);
   }
-  
+
   // returns true if the given student can be found in this course's students list
   boolean containsStudent(Student s) {
     return this.students.contains(s);
